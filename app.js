14e9a4455a9f7be36ec7f4eff349e6f222cbf2f4
Pawel <pawel.html@gmail.com>
2017-11-05 10:02:38 +0100

    register functionality added

--- app.js
+++ app.js
@@ -1,91 +1,3 @@
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-// var exphbs = require('express-handlebars');
-// var expressValidator = require('express-validator');
-// var flash = require('connect-flash');
-// var session = require('express-session');
-// var passport = require('passport');
-// var LocalStrategy = require('passport-local').Strategy;
-// var mongo = require('mongodb');
-// var mongoose = require('mongoose');
-//     mongoose.connect('mongodb://localhost/loginsystem');
-// var db = mongoose.connection;
-
-
-// var routes = require('./routes/index');
-// var users = require('./routes/users');
-
-// // init app //
-// var app = express();
-
-// // View engine //
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
-// app.set('view engine', 'handlebars');
-
-// // BodyParser Middleware //
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// // set static folder //
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // middleware for express session //
-// app.use(session({
-//     secret: 'secret',
-//     saveUninitialized: true,
-//     resave: true
-// }));
-
-// // Passport: //
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Express validator //
-// app.use(expressValidator({
-//     errorFormatter: function(param, msg, value) {
-//         var namespace = param.split('.')
-//         , root    = namespace.shift()
-//         , formParam = root;
-  
-//       while(namespace.length) {
-//         formParam += '[' + namespace.shift() + ']';
-//       }
-//       return {
-//         param : formParam,
-//         msg   : msg,
-//         value : value
-//       };
-//     }
-//   }));
-
-// // Connect flash middleware //
-// app.use(flash());
-
-// // global var for our messeges: //
-// app.use(function(req, res, next){
-//     res.locals.success_msg = req.flash('success_msg');
-//     res.locals.error_msg = req.flash('error_msg');
-//     res.locals.error = req.flash('error');
-//     next();
-// });
-
-// // middleware for routes //
-// app.use('/', routes);
-// app.use('/users', users);
-
-// // set port //
-// app.set('port', (process.env.PORT || 3000));
-
-// app.listen(app.get('port'), function(){
-//     console.log("Server is running on port: "+app.get('port'));
-// });
-
-//  ---------------------------------------------------- //
-
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -99,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
+mongoose.connect('mongodb://localhost/loginsystem');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var expressValidator = require("express-validator");
+router.use(expressValidator());
 
 router.get('/register', function(req, res){
     res.render('register');
 });
+router.post('/register', function(req, res){
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    
+    // validation: 
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('password', 'Password is reqired').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        var newUser = new User({
+            username,
+            email,
+            password
+        });
+        User.createUser(newUser, function(err, user){
+            if(err) throw err;
+            console.log("user form createUser f-n", user);
+        });
+        req.flash('success_msg', "You are registred and can login now");
+        res.redirect('/users/login');
+    }
+})
 
 router.get('/login', function(req, res){
     res.render('login')
-})
+});
+
+app.post("/login", 
+    passport.authenticate("local", {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    }), 
+    function(req, res) {
+        res.redirect('/');
+});
 
 module.exports = router;
